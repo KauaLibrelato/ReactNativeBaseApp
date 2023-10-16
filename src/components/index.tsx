e363c928649b4e5bb2c5c35d87a1bfb4dbe6b853
Kaua Librelato da Costa <kaualibrelatodacosta@gmail.com>
2023-10-16 17:40:15 -0300

    fix: arquivos/linhas desnecessarias

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
-export * from "./Others/Load";
 export * from "./Form/Buttons/GlobalButton";
 export * from "./Form/Buttons/NoFillButton";
-export * from "./Form/Buttons/NoFillWithIconButton";
 export * from "./Form/Inputs/InputText";
 export * from "./Form/Inputs/PasswordInput";
+export * from "./Others/Load";
+
